fd2ef088ec4b32c939adbaf747915be18f0d4f6d
see-simon <60693190+see-simon@users.noreply.github.com>
2022-03-18 00:55:09 +0200

    commit

--- src/Components/Screens/NavMenuScreens/Reports/ReportScreen.js
+++ src/Components/Screens/NavMenuScreens/Reports/ReportScreen.js
@@ -1,12 +1,82 @@
-import React from 'react';
-
-
-
+import React, {useState} from 'react';
+import { send } from 'emailjs-com';
 
 const ReportScreen = () => {
-  return <div>
-      Reports Screen
-  </div>;
+
+  const [toSend, setToSend] = useState({
+    from_name: '',
+    to_name: '',
+    message: '',
+    reply_to: '',
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    send(
+      'service_ygavo3m',
+      'template_jdulvyy',
+      toSend,
+      'TIORF9RNPSLNn6teW'
+
+      // 'SERVICE ID',
+      // 'TEMPLATE ID',
+      // toSend,
+      // 'User ID'
+    )
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+      })
+      .catch((err) => {
+        console.log('FAILED...', err);
+      });
+  };
+
+  const handleChange = (e) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value });
+  };
+
+
+  console.log(toSend)
+ 
+
+  return (
+
+    
+     <form onSubmit={onSubmit}>
+  <input
+    type='text'
+    name='from_name'
+    placeholder='from name'
+    value={toSend.from_name}
+    onChange={handleChange}
+  />
+  <input
+    type='text'
+    name='to_name'
+    placeholder='to name'
+    value={toSend.to_name}
+    onChange={handleChange}
+  />
+  <input
+    type='text'
+    name='message'
+    placeholder='Your message'
+    value={toSend.message}
+    onChange={handleChange}
+  />
+  
+  <input
+    type='text'
+    name='reply_to'
+    placeholder='Your email'
+    value={toSend.reply_to}
+    onChange={handleChange}
+  />
+
+  <button type='submit'>Submit</button>
+</form>
+  
+  )
 };
 
 export default ReportScreen;
